355975dce377d3177d5cca0b20d25571b59d6c47
GitHubのユーザ名 <shuibenshangxi@gmail.com>
2021-11-29 05:40:24 +0000

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 students_array = [
-  { name: '水本', number: '1234', email: 'a@tanaka', password: 'naokimizu'},
-  { name: '川崎', number: '5678', email: 'b@tanaka', password: 'naokimizu'},
-  { name: '大和', number: '9012', email: 'c@tanaka', password: 'naokimizu'},
+  { name: '水本', number: '1234', email: 'a@gmail.com', password: 'naokimizu'},
+  { name: '川崎', number: '5678', email: 'b@gmail.com', password: 'naokimizu'},
+  { name: '大和', number: '9012', email: 'c@gmail.com', password: 'naokimizu'},
 ]
 students_array.each do |data|
   Student.create!(name: data[:name], number: data[:number],email: data[:email], password: data[:password])
 end
 
+teachers_array = [
+  { name: '田中', email: 'd@gmail.com', password: 'naokimizu'},
+  { name: '中田', email: 'e@gmail.com', password: 'naokimizu'},
+  { name: '山本', email: 'f@gmail.com', password: 'naokimizu'},
+]
+teachers_array.each do |data|
+  Teacher.create!(name: data[:name], email: data[:email], password: data[:password])
+end
